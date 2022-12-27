f7eb7ddcbacdd2a98dfe1a79f3fb5ab96a822047
Corey Sawin <sawincp@gmail.com>
2022-12-27 12:53:55 -0500

    commit tree 83888f691a1e7b167e09d46063594e7ec0ff8eb5

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const length = word.length
+
+  for(let i = 0; i <length/2; i++){
+    if(word[i] !== word[length -1 - i]){
+      return false;
+    }
+    return true
+  }
 }
 
 /* 
